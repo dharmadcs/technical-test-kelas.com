15bbfa18a13cda36cf96444c5188cd6b97c78fd5
Harry Hidayat <harryhdtofficial@gmail.com>
2022-09-15 11:18:14 +0700

    second commit

--- index1.php
+++ index1.php
@@ -18,7 +18,26 @@ function hitungBanyakKarakterYangSama($str) {
     $result = "";
     for ($i = 0; $i < strlen($str); $i++) {
         $result .= $str[$i];
-        $charCount = substr_count(STRING, $str[$i]);
+        $charCount = substr_count($str, $str[$i]);
+        if ($charCount > 1) {
+            $result .= $charCount;
+        }
+    }
+    //
+    for ($k = 0; $k < strlen($result); $k++) {
+        if (is_numeric($result[$k])) {
+            $result = str_replace($result[$k], "", $result);
+        }
+    }
+    echo "<br><br>";
+    $stringParts = str_split($result);
+    sort($stringParts);
+    $str = implode($stringParts);
+    //
+    $result = "";
+    for ($i = 0; $i < strlen($str); $i++) {
+        $result .= $str[$i];
+        $charCount = substr_count($str, $str[$i]);
         if ($charCount > 1) {
             $result .= $charCount;
             $i += $charCount - 1;
@@ -28,11 +47,13 @@ function hitungBanyakKarakterYangSama($str) {
 }
 
 // str 1
-echo "str 1<br>Hasil: ";
+echo "str 1 : $str1<br>";
+echo "Hasil: ";
 hitungBanyakKarakterYangSama($str1);
 
 echo "<br><br>";
 
 // str 2
-echo "str 2<br>Hasil: ";
+echo "str 2 : $str2<br>";
+echo "Hasil: ";
 hitungBanyakKarakterYangSama($str2);
